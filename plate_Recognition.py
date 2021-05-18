69d7edde09cb05e929cb31de6708dbe0922d0523
IamNator <natverior1@gmail.com>
2021-05-18 02:03:24 +0100

    ...

--- plate_Recognition.py
+++ plate_Recognition.py
@@ -3,9 +3,9 @@ import sys
 import requests
 # If you are using a Jupyter Notebook, uncomment the following line.
 # %matplotlib inline
-import matplotlib.pyplot as plt
-from matplotlib.patches import Rectangle
-from PIL import Image
+# import matplotlib.pyplot as plt
+# from matplotlib.patches import Rectangle
+# from PIL import Image
 from io import BytesIO
 from config import CONFIG
 from picamera import PiCamera
@@ -45,26 +45,8 @@ for line in line_infos:
             word_infos.append(word_info)
 word_infos
 
-# Display the image and overlay it with the extracted text.
-plt.figure(figsize=(5, 5))
-image = Image.open(BytesIO(requests.get(image_url).content))
-ax = plt.imshow(image, alpha=0.5)
-for word in word_infos:
-    bbox = [int(num) for num in word["boundingBox"].split(",")]
-    text = word["text"]
-    origin = (bbox[0], bbox[1])
-    patch = Rectangle(origin, bbox[2], bbox[3],
-                      fill=False, linewidth=2, color='y')
-    ax.axes.add_patch(patch)
-    plt.text(origin[0], origin[1], text, fontsize=20, weight="bold", va="top")
-plt.show()
-plt.axis("off")
 
-    
-
-
-
-
+print(word_infos)
 
 
 
