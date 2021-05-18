5aa65ad212b2e2a4b7999ad6b53fe5824a3baa57
IamNator <natverior1@gmail.com>
2021-05-18 11:17:59 +0100

    ...

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from plate_recognition import get_plate_number 
 from log_checks import log_check 
-from interface import move
-from interface import is_moving 
+import interface as interface
+# from interface import is_moving 
 
 
 
@@ -9,11 +9,11 @@ from interface import is_moving
 
 i = 0
 while true:
-    if is_moving() == false:
-        move(false)
+    if interface.is_moving() == false:
+        interface.move(false)
         plate_number = get_plate_number()
         packing_space_id = i % 4
         i = i+1
         if log_check(plate_number, packing_space_id) == false:
             continue #repeat the process
-        move(true)        
+        interface.move(true)        
