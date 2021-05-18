3f8b95ff4a92a2771a07cb4ff18c2e65bba4e45b
IamNator <natverior1@gmail.com>
2021-05-18 11:33:10 +0100

    ...

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ from robot_interface import is_moving
 i = 0
 while 1:
     if is_moving() == 0:
-        move(false)
+        move(0)
         plate_number = get_plate_number()
         packing_space_id = i % 4
         i = i+1
         if log_check(plate_number, packing_space_id) == 0:
             continue #repeat the process
-        move(true)        
+        move(1)        
