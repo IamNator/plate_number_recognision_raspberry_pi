a15f9c77a29ced00b287b3fcc3c812a97cfda2bc
IamNator <natverior1@gmail.com>
2021-05-18 11:09:58 +0100

    ...

--- interface.py
+++ interface.py
@@ -7,7 +7,7 @@ inputPIN = 3
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(outputPIN,GPIO.OUT)
 GPIO.setup(inputPIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-
+import get_plate_number 
 
 # returns the state of the robot, moving or not
 def is_moving():
@@ -26,10 +26,10 @@ def move(state):
     stop_motor
         
 
-def start_motor:
+def start_motor():
     GPIO.output(outputPIN, GPIO.HIGH)    
 
-def stop_motor:
+def stop_motor():
     GPIO.output(outputPIN, GPIO.LOW)
       
 
