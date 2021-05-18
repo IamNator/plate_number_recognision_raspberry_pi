0e76ee78354c678f39759a3f9c7d2b30d3b6d16f
IamNator <natverior1@gmail.com>
2021-05-18 11:03:46 +0100

    ...

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ from interface import is_moving
 
 i = 0
 while true:
-    if !is_moving():
+    if is_moving() == false:
         move(false)
         plate_number = get_plate_number()
         packing_space_id = i % 4
         i = i+1
-        if !log_check(plate_number, packing_space_id)
+        if log_check(plate_number, packing_space_id) == false
             continue #repeat the process
         move(true)        
