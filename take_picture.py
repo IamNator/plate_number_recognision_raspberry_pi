b7ed0dc99d955630225c2758577cfb457f496249
IamNator <natverior1@gmail.com>
2021-05-19 16:14:41 +0100

    testing

--- take_picture.py
+++ take_picture.py
@@ -5,15 +5,15 @@ from time import sleep
 
 # takes a picture
 def take_picture():
-    # my_stream = BytesIO()
+    my_stream = BytesIO()
     camera = PiCamera()
     camera.resolution = (1024, 768)
     camera.start_preview()
     # warm up camera
     sleep(1)
-    # camera.capture(my_stream, 'jpeg')
-    camera.capture("/home/pi/Desktop/image.jpeg")
+    camera.capture(my_stream, 'jpeg')
+    # camera.capture("/home/pi/Desktop/image.jpeg")
     camera.stop_preview()
-    return True
+    return my_stream
 
-print(take_picture())
+print(take_picture().read().__sizeof__)
