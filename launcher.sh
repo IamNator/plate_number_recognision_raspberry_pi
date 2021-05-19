42060eb8914d388c61ad2531a9c598045a89f901
IamNator <natverior1@gmail.com>
2021-05-20 00:39:51 +0100

    updating laucher.sh

--- launcher.sh
+++ launcher.sh
@@ -1,6 +1,8 @@
 #!bash
 # launcher.sh
 # navigate to project directory, pull from github and run project code
-cd /home/pi/Plate\ Recognition && git pull && sudo python3 main.py >> /home/pi/Desktop/log.txt 2>&1 &   
+cd /home/pi/Plate\ Recognition 
+git pull 
+sudo python3 main.py >> /home/pi/Desktop/log.txt 2>&1 &   
 exit 0
 
