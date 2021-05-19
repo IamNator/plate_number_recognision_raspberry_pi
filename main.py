7eb6a849ed73fdeeec16e042d42a09b72484af77
IamNator <natverior1@gmail.com>
2021-05-19 14:44:34 +0100

    bug fixed

--- main.py
+++ main.py
@@ -9,21 +9,13 @@ from robot_interface import is_moving
 
 print("starting ...\n")
 i = 0
-# while True:
-#     if not is_moving():
-#         print("plate recognition...\n")
-#         move(False)
-#         plate_number = plate_recognition.get_plate_number()
-#         packing_space_id = i % 4
-#         i = i+1
-#         if not log_checks.log_check(plate_number, packing_space_id):
-#             continue #repeat the process
-#         move(True) 
-
 while True:
-    move(True)
-    print("move..")
-    time.sleep(1)
-    move(False)
-    time.sleep(1)
-    print("stop...")
+    if not is_moving():
+        print("plate recognition...\n")
+        move(False)
+        plate_number = plate_recognition.get_plate_number()
+        packing_space_id = i % 4
+        i = i+1
+        if not log_checks.log_check(plate_number, packing_space_id):
+            continue #repeat the process
+        move(True) 
