f1aa87f19eafabfa6905511d96759c11106738d5
IamNator <natverior1@gmail.com>
2021-05-19 11:24:27 +0100

    ...

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from plate_recognition import get_plate_number
-from log_checks import log_check 
+import plate_recognition as plate_recognition
+import log_checks 
 from robot_interface import move
 from robot_interface import is_moving
 
@@ -10,10 +10,10 @@ while True:
     if is_moving() == 0:
         print("plate recognition...\n")
         move(False)
-        plate_number = get_plate_number()
+        plate_number = plate_recognition.get_plate_number()
         packing_space_id = i % 4
         i = i+1
-        if not log_check(plate_number, packing_space_id):
+        if not log_checks.log_check(plate_number, packing_space_id):
             continue #repeat the process
         move(True) 
     
