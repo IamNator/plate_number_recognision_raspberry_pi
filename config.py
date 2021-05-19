357a9395f3d364809c3d9da7e66ecde3590dcc54
IamNator <natverior1@gmail.com>
2021-05-19 20:21:18 +0100

    ...

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 CONFIG = {"COMPUTER_VISION_SUBSCRIPTION_KEY":"5c0c98e9523d457993ca06f68c9951f5", 
-          "COMPUTER_VISION_ENDPOINT":"https://plate-ocr-mawaqif.cognitiveservices.azure.com", 
+          "COMPUTER_VISION_ENDPOINT":"https://plate-ocr-mawaqif.cognitiveservices.azure.com/vision/v3.2/ocr?language=unk&detectOrientation=true&model-version=latest", 
           "MAWAQIF_UPLOAD_CHECKS":"https://mawaqif.herokuapp.com/api/checks/add", 
           "NUMBER_OF_PACKING_SPACES":5}
