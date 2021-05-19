e6b11b3b5a2078b26957b59fec95d249a40f6056
IamNator <natverior1@gmail.com>
2021-05-19 13:03:53 +0100

    testing out move

--- main.py
+++ main.py
@@ -2,20 +2,25 @@
 
 import plate_recognition as plate_recognition
 import log_checks 
+import time
 from robot_interface import move
 from robot_interface import is_moving
 
 
 print("starting ...\n")
 i = 0
+# while True:
+#     if not is_moving():
+#         print("plate recognition...\n")
+#         move(False)
+#         plate_number = plate_recognition.get_plate_number()
+#         packing_space_id = i % 4
+#         i = i+1
+#         if not log_checks.log_check(plate_number, packing_space_id):
+#             continue #repeat the process
+#         move(True) 
+
 while True:
-    # if not is_moving():
-    #     print("plate recognition...\n")
-    #     move(False)
-    #     plate_number = plate_recognition.get_plate_number()
-    #     packing_space_id = i % 4
-    #     i = i+1
-    #     if not log_checks.log_check(plate_number, packing_space_id):
-    #         continue #repeat the process
-        move(True) 
-    
+    move(True)
+    time.sleep(3)
+    move(False)
