ae882845b48550cafded22b0c7568da5cd0b2c75
IamNator <natverior1@gmail.com>
2021-05-20 00:55:57 +0100

    ...

--- launcher.sh
+++ launcher.sh
@@ -3,6 +3,6 @@
 # navigate to project directory, pull from github and run project code
 cd /home/pi/Plate\ Recognition 
 git pull 
-sudo python3 main.py >> /home/pi/Desktop/log.txt 2>&1 &   
+python3 main.py | >> /home/pi/Desktop/log.txt &   
 exit 0
 
