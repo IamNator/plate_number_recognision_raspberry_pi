9a7903bd161ec9163ece26c40cb86cba4f5413a2
IamNator <natverior1@gmail.com>
2021-05-19 19:13:42 +0100

    testing log_checks.py

--- log_checks.py
+++ log_checks.py
@@ -21,7 +21,7 @@ def log_check(plate_number, packing_space_id ):
     headers = {'Content-Type': 'application/json'}
     # put the byte array into your post request
     isEmpty = is_empty(plate_number)
-    time_of_check = default(datetime.datetime.now().fromisoformat())
+    time_of_check = default(datetime.datetime.now())
     
     checks_log = {}
     checks_log["plate_number"] = plate_number
