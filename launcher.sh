7508ec3fff11f491d3b69d0ac359163f65c6e44f
IamNator <natverior1@gmail.com>
2021-05-21 13:34:44 +0100

    ...

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,10 @@
 #!/bin/sh 
 #
 #cd into project directory, pull from github  and run the pi code
-# 
-cd /home/pi/Plate\ Recognition 
+
+cd /home/"pi/Plate Recognition" 
 git pull 
 echo "starts " > /home/pi/Desktop/log.txt
-python3 main.py | >> /home/pi/Desktop/log.txt
+python3 main.py | >> /home/pi/Desktop/log.txt &
+
+exit 0
