a4ba821f5f060ac9ce9996bae710a7e17b608f41
IamNator <natverior1@gmail.com>
2021-05-21 11:56:56 +0100

    editting launch.sh

--- launcher.sh
+++ launcher.sh
@@ -1,12 +1,7 @@
-
-#!/bin/sh -e
-#
-#rc.local
+#!/bin/sh 
 #
 #
 # navigate to project directory, pull from github and run project code
 cd /home/pi/Plate\ Recognition 
 git pull 
 python3 main.py | >> /home/pi/Desktop/log.txt &   
-exit 0
-
